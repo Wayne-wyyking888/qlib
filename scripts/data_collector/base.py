d4d9991ab2e9d4441d5befa17e5e3d4161970daf
Wayne-wyyking888 <154812845+Wayne-wyyking888@users.noreply.github.com>
2024-03-19 12:41:01 -0500

    Update base.py

--- scripts/data_collector/base.py
+++ scripts/data_collector/base.py
@@ -290,23 +290,25 @@ class Normalize:
 
         # some symbol_field values such as TRUE, NA are decoded as True(bool), NaN(np.float) by pandas default csv parsing.
         # manually defines dtype and na_values of the symbol_field.
-        default_na = pd._libs.parsers.STR_NA_VALUES  # pylint: disable=I1101
-        symbol_na = default_na.copy()
-        symbol_na.remove("NA")
-        columns = pd.read_csv(file_path, nrows=0).columns
-        df = pd.read_csv(
-            file_path,
-            dtype={self._symbol_field_name: str},
-            keep_default_na=False,
-            na_values={col: symbol_na if col == self._symbol_field_name else default_na for col in columns},
-        )
+        try: 
+            default_na = pd._libs.parsers.STR_NA_VALUES  # pylint: disable=I1101
+            symbol_na = default_na.copy()
+            symbol_na.remove("NA")
+            columns = pd.read_csv(file_path, nrows=0).columns
+            df = pd.read_csv(
+                file_path,
+                dtype={self._symbol_field_name: str},
+                keep_default_na=False,
+                na_values={col: symbol_na if col == self._symbol_field_name else default_na for col in columns},
+            )
 
-        df = self._normalize_obj.normalize(df)
-        if df is not None and not df.empty:
-            if self._end_date is not None:
-                _mask = pd.to_datetime(df[self._date_field_name]) <= pd.Timestamp(self._end_date)
-                df = df[_mask]
-            df.to_csv(self._target_dir.joinpath(file_path.name), index=False)
+            df = self._normalize_obj.normalize(df)
+            if df is not None and not df.empty:
+                if self._end_date is not None:
+                    _mask = pd.to_datetime(df[self._date_field_name]) <= pd.Timestamp(self._end_date)
+                    df = df[_mask]
+                df.to_csv(self._target_dir.joinpath(file_path.name), index=False)
+        except: return
 
     def normalize(self):
         logger.info("normalize data......")
